20f4b48373f41fea39323d3a7aefcdd401cd0bf6
RebalancedShowdown <66094079+RebalancedShowdown@users.noreply.github.com>
2020-08-24 22:34:26 -0700

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -94,9 +94,9 @@ export const BattleMovedex: {[moveid: string]: MoveData} = {
 			if (!target.hasType('Steel')) {
 				this.ignoreImmunity['Poison'] = true;
 			},	
-			onEffectiveness(typeMod, target, type) {
-				if (type === 'Steel')  return 1;
-			},
+		},
+		onEffectiveness(typeMod, target, type) {
+			if (type === 'Steel')  return 1;
 		},
 		target: "allAdjacentFoes",
 		type: "Poison",
