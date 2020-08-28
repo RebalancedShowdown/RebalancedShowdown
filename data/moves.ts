03640bc44134fcd60e863d856e0168105fb546d1
RebalancedShowdown <66094079+RebalancedShowdown@users.noreply.github.com>
2020-08-27 21:07:28 -0700

    Update moves.ts
    updated sacred fire

--- data/moves.ts
+++ data/moves.ts
@@ -15207,7 +15207,7 @@ export const BattleMovedex: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, mirror: 1, defrost: 1},
 		secondary: {
-			chance: 50,
+			chance: 100,
 			status: 'brn',
 		},
 		target: "normal",
